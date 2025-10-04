24b6edd97c55c7b272b6a9f0afa298d6928b5fcb
Яковлев Данило <166537891+YakovlevDanylo@users.noreply.github.com>
2025-10-04 15:15:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -10,6 +10,38 @@ display.set_caption("Моя перша гра")
 # Створення годинника для кадрів
 clock = time.Clock()
 
+class Block:
+    def __init__(self, img_path, width, height):
+        self.image = image.load(img_path)
+        self.image = transform.scale(self.image, (width, height))
+        self.height = height
+        self.width = width
+        # Створення хіт боксу
+        self.rect = self.image.get_rect()
+        self.rect.x = 0
+        self.rect.y = 0
+        blocks.append(self)
+
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+blocks = []
+big_rock = Block("asset_level/big_rock.png", 150, 150)
+medium_block = Block("asset_level/medium_rock.png", 150, 100)
+for i in range(5):
+    Block("asset_level/full_g_block.png", 200,60)
+for i in range(5):
+    Block("asset_level/full_h_block.png", 60,200)
+for i in range(5):
+    Block("asset_level/one_block.png", 60,60)
+for i in range(5):
+    Block("asset_level/little_bush.png", 60,60)
+for i in range(5):
+    Block("asset_level/little_leaf.png", 60,60)
+for i in range(5):
+    Block("asset_level/sprout_2.png", 60,60)
+for i in range(5):
+    Block("asset_level/big_stick.png", 150,250)
 
 
 # Ігровий цикл
@@ -21,6 +53,8 @@ while True:
             quit()
 
     # 2 - Малювання усіх об'єктів на екрані
+    for block in blocks:
+        block.reset()
 
     # 3 - Оновлення екрану
     display.update()
